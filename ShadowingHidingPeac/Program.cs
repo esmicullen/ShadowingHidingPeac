b29b1d4b7bd777831ec26e05b07da6cf749f3bda
gahyun <s2112@e-mirim.hs.kr>
2023-06-12 13:34:46 +0900

    변수에서의 하이딩

--- ShadowingHidingPeac/Program.cs
+++ ShadowingHidingPeac/Program.cs
@@ -8,11 +8,25 @@ namespace ShadowingHidingPeac
 {
     internal class Program
     {
+        class Parent
+        {
+            public int varoable = 273;
+        }
+
+        class Child : Parent 
+        {
+            public new string varoable = "shadowing";
+        }
         public static int number = 10;
         static void Main(string[] args)
         {
             int number = 20;
             Console.WriteLine(number);
+            Child child = new Child();
+            Console.WriteLine(child.varoable);
+            Parent p = new Parent();
+            Console.WriteLine(p.varoable);
+
         }
     }
 }
